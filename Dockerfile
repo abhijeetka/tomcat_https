dae82214120147b61dd4076523713c1461d36b9f
Abhijeet <abhijeet.kamble@cdk.com>
2019-11-12 11:21:43 +0530

    added final changes

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+FROM registry.psgit.bankofthewest.com/docker/images/botw_rhel7:latest
+
+MAINTAINER sandeep.chitte@bankofthewest.com
+
+USER root
+
+# Install prepare infrastructure
+RUN yum -y update && \
+ yum -y install wget && \
+ yum -y install tar
+
+# Prepare environment 
+ENV JAVA_HOME /opt/java
+ENV CATALINA_HOME /usr/local/tomcat 
+ENV PATH $PATH:$JAVA_HOME/bin:$CATALINA_HOME/bin:$CATALINA_HOME/scripts
+
+RUN yum update -y ; yum install -y  java-1.8.0-openjdk ; yum clean all
+
+# Install Tomcat
+ENV TOMCAT_MAJOR 8
+ENV TOMCAT_VERSION 8.5.35
+
+RUN mkdir -p /usr/local/tomcat
+COPY apache-tomcat-8.5.23.tar.gz .
+RUN tar xvf apache-tomcat-8.5.23.tar.gz -C ${CATALINA_HOME} --strip-components=1
+
+COPY server.xml ${CATALINA_HOME}/conf/server.xml
+RUN chmod +x ${CATALINA_HOME}/bin/*sh
+
+
+ADD run.sh /run.sh
+RUN chmod +x /run.sh
+
+# Create tomcat user
+RUN groupadd -r tomcat && \
+ useradd -g tomcat -d ${CATALINA_HOME} -s /sbin/nologin  -c "Tomcat user" tomcat && \
+ chown -R tomcat:tomcat ${CATALINA_HOME}
+
+WORKDIR /opt/tomcat
+
+EXPOSE 8080
+EXPOSE 8443
+
+USER tomcat
+CMD ["run.sh"]
---
+++ run.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+
+cat >${CATALINA_HOME}/conf/tomcat-users.xml <<EOL
+<?xml version="1.0" encoding="utf-8"?>
+<tomcat-users>
+  <role rolename="admin-gui"/>
+  <role rolename="admin-script"/>
+  <role rolename="manager-gui"/>
+  <role rolename="manager-status"/>
+  <role rolename="manager-script"/>
+  <user name="admin" password="$ADMIN_PASSWORD"
+    roles="admin-gui,admin-script,manager-gui,manager-status,manager-script"/>
+</tomcat-users>
+EOL
+
+echo "========================================================================="
+echo "You can now connect to this instance using:"
+echo
+echo "    user name: admin"
+echo "    password : $ADMIN_PASSWORD"
+echo
+echo "========================================================================"
+
+# If the webapps directory is empty (the user has specified a volume), copy the
+# contents from the folder in tmp (which is created when the image was built).
+WEBAPPS_HOME="${CATALINA_HOME}/webapps"
+WEBAPPS_TMP="/tmp/webapps"
+
+if [ ! "$(ls -A $WEBAPPS_HOME)" ]; then
+    cp -r $WEBAPPS_TMP/* $WEBAPPS_HOME
+fi
+
+
+echo
+echo "========================================================================="
+echo "Using certificate password: $CERT_PASSWORD"
+echo "========================================================================"
+
+if [[ -z "$DNAME" ]]; then
+    DNAME='CN=www.botw.com, OU=chitte, O=Sandeep, C=IN'
+fi
+# Generate Self-Signed SSL certificate in a new keystore
+keytool -genkey -noprompt \
+-alias selfsigned \
+-dname "$DNAME" \
+-keyalg RSA \
+-storepass $CERT_PASSWORD \
+-keypass $CERT_PASSWORD \
+-validity 360 \
+-keysize 2048 \
+-keystore keystore.jks
+
+# Uncomment SSL section in server.xml
+# and insert SSL certificate information
+sed -i '$!N;s/<!--\s*\n\s*<Connector port="8443"/<Connector port="8443" keyAlias="selfsigned" \
+               keystoreFile="\/keystore.jks" keystorePass="'$CERT_PASSWORD'"/g;P;D' \
+               ${CATALINA_HOME}/conf/server.xml
+
+sed -i '$!N;s/clientAuth="false" sslProtocol="TLS" \/>\n\s*-->/clientAuth="false" sslProtocol="TLS" \/>/g;P;D' \
+${CATALINA_HOME}/conf/server.xml
+
+catalina.sh run
